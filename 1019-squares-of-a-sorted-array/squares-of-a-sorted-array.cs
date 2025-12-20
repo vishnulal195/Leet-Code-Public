4e025dad29d5999c0c052bfb898b090eccc8854e
vishnulal195 <144610148+vishnulal195@users.noreply.github.com>
2025-12-20 20:01:08 +0530

    Time: 1 ms (100.00%) | Memory: 65.2 MB (15.29%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cs
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cs
@@ -1,10 +1,25 @@
 public class Solution {
     public int[] SortedSquares(int[] nums) {
-        for(int i=0;i<nums.Length;i++)
-        {
-            nums[i]=nums[i]*nums[i];
+       int n = nums.Length;
+        int[] result = new int[n];
+
+        int left = 0;
+        int right = n - 1;
+        int index = n - 1;
+
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare > rightSquare) {
+                result[index--] = leftSquare;
+                left++;
+            } else {
+                result[index--] = rightSquare;
+                right--;
+            }
         }
-        Array.Sort(nums);  
-        return nums;
+
+        return result;
     }
 }
