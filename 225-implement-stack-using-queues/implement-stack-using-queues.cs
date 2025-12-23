43cd938bddff9c60665fe47907b7876a83c05980
vishnulal195 <144610148+vishnulal195@users.noreply.github.com>
2025-12-23 20:20:30 +0530

    Time: 1 ms (79.72%) | Memory: 43.7 MB (77.62%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cs
@@ -0,0 +1,36 @@
+public class MyStack {
+ private Queue<int> queue;
+    public MyStack() {
+       queue = new Queue<int>(); 
+    }
+    
+    public void Push(int x) {
+        queue.Enqueue(x);
+        int size = queue.Count;
+        for (int i = 0; i < size - 1; i++)
+        {
+            queue.Enqueue(queue.Dequeue());
+        }
+    }
+    
+    public int Pop() {
+        return queue.Dequeue();
+    }
+    
+    public int Top() {
+        return queue.Peek();
+    }
+    
+    public bool Empty() {
+         return queue.Count == 0;
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.Push(x);
+ * int param_2 = obj.Pop();
+ * int param_3 = obj.Top();
+ * bool param_4 = obj.Empty();
+ */
